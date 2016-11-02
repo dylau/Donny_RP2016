4c9923c86ccceda37bda551db50d43f7c1cb8151
dylau <dyliu.lyon@gmail.com>
2016-11-02 13:09:13 +0100

    XML syntax updated

--- ITECH_RP1516-DonnyBranch/SyntaxGeneration/TaskList/StepXml.cs
+++ ITECH_RP1516-DonnyBranch/SyntaxGeneration/TaskList/StepXml.cs
@@ -27,35 +27,55 @@ namespace RP1516
         {
             // digit control
             string Step3Digit = null;
+            string Step4Digit = null;
+
             if (Step < 10)
+            {
+                Step4Digit = String.Format("000{0}", Step);
                 Step3Digit = String.Format("00{0}", Step);
+            }
+
 
             if (10 <= Step && Step < 100)
+            {
+                Step4Digit = String.Format("00{0}", Step);
                 Step3Digit = String.Format("0{0}", Step);
+            }
 
             if (100 <= Step && Step < 1000)
-                Step3Digit = Step.ToString();
+            {
+                Step4Digit = String.Format("0{0}", Step);
+                Step3Digit = String.Format("{0}", Step);
+            }
+
+            if (1000 <= Step && Step < 10000)
+            {
+                Step4Digit = Step.ToString();
+            }
 
             // settings 
             XmlWriterSettings Settings = new XmlWriterSettings();
             Settings.Indent = true;
             Settings.IndentChars = "\t";
             Settings.OmitXmlDeclaration = true;
-
             XmlDocument xmlDoc = new XmlDocument(); // This is the xml file of 1 fiber.
             
-            // schema
-            // rootNode, step general info  
-
+            // stepNode
             XmlNode stepNode = xmlDoc.CreateElement("Step");
 
-            //
+            // no neighbours
             XmlAttribute stepID = xmlDoc.CreateAttribute("id");
-            stepID.Value = Step3Digit.ToString();
+            stepID.Value = Step4Digit.ToString();
             stepNode.Attributes.Append(stepID);
 
+
+            XmlAttribute Wind_pin = xmlDoc.CreateAttribute("wind_pin");
+
+            string OperationSide = null;
             if (StepCurve.Direction == "AB")
             {
+                OperationSide = "B";
+
                 XmlAttribute PinStart = xmlDoc.CreateAttribute("pinStart");
                 PinStart.Value = StepCurve.PinA.PinID;
                 stepNode.Attributes.Append(PinStart);
@@ -63,10 +83,15 @@ namespace RP1516
                 XmlAttribute PinEnd = xmlDoc.CreateAttribute("pinEnd");
                 PinEnd.Value = StepCurve.PinB.PinID;
                 stepNode.Attributes.Append(PinEnd);
+
+                Wind_pin.Value = string.Format("W_{0}", StepCurve.PinB.Index.ToString());
+
             }
 
             if (StepCurve.Direction == "BA")
             {
+                OperationSide = "A";
+
                 XmlAttribute PinStart = xmlDoc.CreateAttribute("pinStart");
                 PinStart.Value = StepCurve.PinB.PinID;
                 stepNode.Attributes.Append(PinStart);
@@ -74,50 +99,52 @@ namespace RP1516
                 XmlAttribute PinEnd = xmlDoc.CreateAttribute("pinEnd");
                 PinEnd.Value = StepCurve.PinA.PinID;
                 stepNode.Attributes.Append(PinEnd);
-            }
 
-
-            XmlAttribute Wind_pin = xmlDoc.CreateAttribute("wind_pin");
-            if (StepCurve.Direction == "AB")
-                Wind_pin.Value = string.Format("W_{0}", StepCurve.PinB.Index.ToString());
-            else
                 Wind_pin.Value = string.Format("W_{0}", StepCurve.PinA.Index.ToString());
+
+            }
             stepNode.Attributes.Append(Wind_pin);
 
-
+            // stepNode done
             xmlDoc.AppendChild(stepNode);
 
+            
+
             // Drone
-            TaskNode droneNode_0 = new TaskNode("0", "Drone", StepCurve.Direction, xmlDoc); 
-            stepNode.AppendChild(droneNode_0.WriteTaskNode());
+            TaskNode Node_0 = new TaskNode("0", "Drone", StepCurve.Direction, xmlDoc); 
+            stepNode.AppendChild(Node_0.WriteTaskNode());
 
-            // Robot
-            TaskNode robotNode_1 = new TaskNode("1", string.Format("Robot{0}", StepCurve.EndPinID[0]), "50", xmlDoc); 
-            stepNode.AppendChild(robotNode_1.WriteTaskNode());
+            // Effectors: Gripper
+            TaskNode Node_1 = new TaskNode("1", "Effectors", string.Format("G{0}OPEN", OperationSide), xmlDoc);
+            stepNode.AppendChild(Node_1.WriteTaskNode());
 
-            // Grip
-            TaskNode robotNode_2 = new TaskNode("2", string.Format("Gripper{0}", StepCurve.EndPinID[0]), "On", xmlDoc);
-            stepNode.AppendChild(robotNode_2.WriteTaskNode());
+            // Robot: 
+            TaskNode Node_2 = new TaskNode("2", string.Format("Robot{0}", OperationSide), "50", xmlDoc); 
+            stepNode.AppendChild(Node_2.WriteTaskNode());
+
+            // Effectors: Gripper
+            TaskNode Node_3 = new TaskNode("3", "Effectors", string.Format("G{0}CLOSE", OperationSide), xmlDoc);
+            stepNode.AppendChild(Node_3.WriteTaskNode());
 
             // Magnet: off
-            TaskNode robotNode_3 = new TaskNode("3", "Magnet", "Off", xmlDoc); 
-            stepNode.AppendChild(robotNode_3.WriteTaskNode());
+            TaskNode Node_4 = new TaskNode("4", "Drone", "MOFF", xmlDoc); 
+            stepNode.AppendChild(Node_3.WriteTaskNode());
 
             // Tension 
-            TaskNode robotNode_4 = new TaskNode("4", "Tension", "On", xmlDoc); 
-            stepNode.AppendChild(robotNode_4.WriteTaskNode());
+            TaskNode Node_5 = new TaskNode("5", "Effectors", "TON", xmlDoc); 
+            stepNode.AppendChild(Node_5.WriteTaskNode());
 
             // Robot: out with effector
-            TaskNode robotNode_5 = new TaskNode("5", string.Format("Robot{0}", StepCurve.EndPinID[0]), "61", xmlDoc);
-            stepNode.AppendChild(robotNode_5.WriteTaskNode());
+            TaskNode Node_6 = new TaskNode("6", string.Format("Robot{0}", OperationSide), "61", xmlDoc);
+            stepNode.AppendChild(Node_6.WriteTaskNode());
 
             // Robot: travel forward
-            TaskNode robotNode_6 = new TaskNode("6", string.Format("Robot{0}", StepCurve.EndPinID[0]), "70", xmlDoc);
-            stepNode.AppendChild(robotNode_6.WriteTaskNode());
+            TaskNode Node_7 = new TaskNode("7", string.Format("Robot{0}", OperationSide), "70", xmlDoc);
+            stepNode.AppendChild(Node_7.WriteTaskNode());
 
             // Robot: wind 
-            TaskNode robotNode_7 = new TaskNode("7", string.Format("Robot{0}", StepCurve.EndPinID[0]), "90", xmlDoc);
-            stepNode.AppendChild(robotNode_7.WriteTaskNode());
+            TaskNode Node_8 = new TaskNode("8", string.Format("Robot{0}", OperationSide), "90", xmlDoc);
+            stepNode.AppendChild(Node_7.WriteTaskNode());
 
             /*
             TaskNode robotNode_7 = new TaskNode("7", string.Format("Robot{0}", StepCurve.EndPinID[0]), string.Format("W_{0}", StepCurve.PinB.Index.ToString()), xmlDoc);
@@ -127,32 +154,32 @@ namespace RP1516
             */
 
             // Robot: travel back
-            TaskNode robotNode_8 = new TaskNode("8", string.Format("Robot{0}", StepCurve.EndPinID[0]), "80", xmlDoc);
-            stepNode.AppendChild(robotNode_8.WriteTaskNode());
+            TaskNode Node_9 = new TaskNode("9", string.Format("Robot{0}", OperationSide), "80", xmlDoc);
+            stepNode.AppendChild(Node_9.WriteTaskNode());
 
             // Robot: in with effector
-            TaskNode robotNode_9 = new TaskNode("9", string.Format("Robot{0}", StepCurve.EndPinID[0]), "51", xmlDoc);
-            stepNode.AppendChild(robotNode_9.WriteTaskNode());
+            TaskNode Node_10 = new TaskNode("10", string.Format("Robot{0}", OperationSide), "51", xmlDoc);
+            stepNode.AppendChild(Node_10.WriteTaskNode());
 
             // Magnet: on
-            TaskNode robotNode_10 = new TaskNode("10", "Magnet", "On", xmlDoc);
-            stepNode.AppendChild(robotNode_10.WriteTaskNode());
+            TaskNode Node_11 = new TaskNode("11", "Drone", "MON", xmlDoc);
+            stepNode.AppendChild(Node_11.WriteTaskNode());
 
             // Tension 
-            TaskNode robotNode_11 = new TaskNode("11", "Tension", "Off", xmlDoc);
-            stepNode.AppendChild(robotNode_11.WriteTaskNode());
+            TaskNode Node_12 = new TaskNode("12", "Effectors", "TOFF", xmlDoc);
+            stepNode.AppendChild(Node_12.WriteTaskNode());
 
             // Gripper
-            TaskNode robotNode_12 = new TaskNode("12", string.Format("Gripper{0}", StepCurve.EndPinID[0]), "Off", xmlDoc);
-            stepNode.AppendChild(robotNode_12.WriteTaskNode());
+            TaskNode Node_13 = new TaskNode("13", "Effectors", string.Format("G{0}OPEN", OperationSide), xmlDoc);
+            stepNode.AppendChild(Node_13.WriteTaskNode());
 
             // Robot: out without effector
-            TaskNode robotNode_13 = new TaskNode("13", string.Format("Robot{0}", StepCurve.EndPinID[0]), "60", xmlDoc);
-            stepNode.AppendChild(robotNode_13.WriteTaskNode());
+            TaskNode Node_14 = new TaskNode("14", string.Format("Robot{0}", OperationSide), "60", xmlDoc);
+            stepNode.AppendChild(Node_14.WriteTaskNode());
 
             // writing and namming the xml file
             //XmlWriter write = XmlWriter.Create(string.Format("C:\\Users\\dyliu\\desktop\\TaskList\\TaskList_{0}.xml", Step3Digit), Settings);
-            XmlWriter write = XmlWriter.Create(string.Format("{0}\\TaskList_{1}.xml", path, Step3Digit), Settings);
+            XmlWriter write = XmlWriter.Create(string.Format("{0}\\TaskList_{1}.xml", path, Step4Digit), Settings);
 
             xmlDoc.Save(write);
         }
